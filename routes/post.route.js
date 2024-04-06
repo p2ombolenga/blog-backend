9e7ddad774f41dc8600d753baeee015b224ef530
p2ombolenga <nsabimanapeter2000@gmail.com>
2024-04-06 18:26:10 +0200

    impelementing post deletion

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createPost, getPosts, getApost, updatePost } from "../controllers/post.controller.js"
+import { createPost, getPosts, getApost, updatePost, deletePost} from "../controllers/post.controller.js"
 
 const postRouter = express.Router()
 
@@ -7,5 +7,6 @@ postRouter.get('/', getPosts);
 postRouter.get('/:id', getApost);
 postRouter.post('/:id', createPost);
 postRouter.put('/:id', updatePost);
+postRouter.delete('/:id', deletePost);
 
 export default postRouter;
